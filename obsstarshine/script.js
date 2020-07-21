14cf322f26f1170c205891f9ffcf867886225e4c
Veloxization <32390461+Veloxization@users.noreply.github.com>
2020-07-21 04:57:48 +0300

    Add files via upload
    Decorations for OBS background

---
+++ obsstarshine/script.js
@@ -0,0 +1,27 @@
+var width = 1920;
+var height = 1080;
+var starNum = 100;
+
+starBrightening = [];
+
+for (var i = 0; i < starNum; i++) {
+	var x = Math.floor(Math.random() * width);
+	var y = Math.floor(Math.random() * height);
+	var startOpacity = Math.random();
+	document.getElementById("wrapper").innerHTML += "<img src='star.png' class='star' style='opacity: " + startOpacity + ";' />";
+	document.getElementsByClassName("star")[i].style.left = x + "px";
+	document.getElementsByClassName("star")[i].style.top = y + "px";
+	starBrightening[i] = true;
+}
+
+var stars = document.getElementsByClassName("star");
+var interval = setInterval(function() {
+	for (var i = 0; i < stars.length; i++) {
+		var increment = 0.01;
+		if (!starBrightening[i]) increment *= -1;
+		var opacity = parseFloat(stars[i].style.opacity) + increment;
+		if (opacity >= 1) starBrightening[i] = false;
+		else if (opacity <= 0) starBrightening[i] = true;
+		stars[i].style.opacity = opacity;
+	}
+}, 10);
